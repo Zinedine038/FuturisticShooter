b629d8dae623188dd3d94bda381ca8c5f902403e
Zinedine038 <zsghari@hotmail.com>
2017-02-20 09:05:11 +0100

    Base weapon foundation

---
+++ FuturisticShooter/Assets/Systems/WeaponBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum WeaponType
+{
+    Automatic,
+    SemiAuto,
+    Mechanical,
+}
+public class WeaponBase : MonoBehaviour
+{
+    public string weaponName;
+    public bool isMelee;
+    protected bool mayAttack;
+
+    public virtual void DoAttack(WeaponType fireMode)
+    {
+        //TODO Ranged Attacks
+    }
+
+    public virtual void DoAttack(bool melee)
+    {
+        //TODO Melee Shenanigans
+    }
+
+    public virtual IEnumerator WaitForAttack(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        mayAttack=true;
+    }
+
+    public virtual void Activate()
+    {
+        //TODO Animations
+    }
+}
